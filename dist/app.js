84b1b61cdfefb034200771f80982971c9ad6c7c7
ravi <samcollins9899@gmail.com>
2022-11-01 22:57:41 +0530

    added writeFileSync

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,32 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = require("http");
+const fs_1 = require("fs");
+const fs = require('fs');
 const port = 3000;
 const server = (0, http_1.createServer)((req, res) => {
     const url = req.url;
+    const method = req.method;
     if (url === "/") {
-        res.setHeader("Content-Type", "text/html");
         res.write("<html>");
         res.write("<head><title>Enter message</title></head>");
-        res.write('<body><form action="/message" method="POST"> <input type="text"><button type="submit">Submit</button></form> </body>');
+        res.write('<body><form action="/message" method="POST"> <input type="text" name="message"><button type="submit">Submit</button></form> </body>');
         res.write("</html>");
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            (0, fs_1.writeFileSync)('message.txt', message);
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
     res.setHeader("Content-Type", "text/html");
--- src/app.ts
+++ src/app.ts
@@ -3,22 +3,43 @@ import { writeFile, writeFileSync } from "fs";
 const fs = require('fs');
 
 
+
+
 const port = 3000;
 const server = createServer((req, res) => {
   const url = req.url;
   const method = req.method;
   if (url === "/") {
-    res.setHeader("Content-Type", "text/html");
+    
     res.write("<html>");
     res.write("<head><title>Enter message</title></head>");
-    res.write('<body><form action="/message" method="POST"> <input type="text"><button type="submit">Submit</button></form> </body>');
+    res.write('<body><form action="/message" method="POST"> <input type="text" name="message"><button type="submit">Submit</button></form> </body>');
     res.write("</html>");
   return  res.end();
   }
 
   if( url === '/message' && method ==='POST')
   {
-     writeFileSync('message.txt', 'Dummy')
+    const body:Array<Buffer>= [];
+  
+     req.on('data', (chunk: Buffer)=>{
+         console.log(chunk);
+         body.push(chunk);
+     })
+     
+     
+
+     req.on('end', ()=>{
+   
+      const parsedBody = Buffer.concat(body).toString();
+      
+      const message = parsedBody.split('=')[1];
+      writeFileSync('message.txt', message)
+     })
+     
+     res.statusCode =302;
+     res.setHeader ('Location', '/');
+     return res.end();
   }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
